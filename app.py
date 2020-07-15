173df4ac9fbdef0f81c3d4fdea1f4b75ff4da979
niekl <niekl.internal@internal.com>
2020-07-15 23:15:07 +0800

    add form code in html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, current_app
 from flask import render_template
 
 app = Flask(__name__)
@@ -7,6 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+
+@app.route('/favicon.ico')
+def get_fav():
+    print(__name__)
+    return current_app.send_static_file('img/favicon.ico')
 
 
 if __name__ == '__main__':
